92e0b57f899a66faa0306cda97c8de40efa72037
vincent <vincent@fd6709c4-d1e2-46c1-9206-fa2d5ecda316>
2008-03-24 11:25:44 +0000

    Maybe functioning launcher and began real server_node, still need to be debugged
    git-svn-id: svn://127.0.0.1/jim/trunk@76 fd6709c4-d1e2-46c1-9206-fa2d5ecda316

--- src/eis.erl
+++ src/eis.erl
@@ -19,6 +19,8 @@
 			init/1
 		]).
 
+-vsn( p01 ).
+
 make_specserv( Module, Func, Args ) ->
 	{Module,
 		{Module, Func, Args},
@@ -27,23 +29,13 @@ make_specserv( Module, Func, Args ) ->
 		worker,
 		[Module]}.
 
-make_specnalance( Module, Func, Args ) ->
+make_specbalance( Module, Func, Args ) ->
 	{Module,
 		{Module, Func, Args},
 		permanent,
 		1000,
 		supervisor,
 		[Module]}.
-		
-balanceLaunch( Module, Func, Max, LogInfo ) ->
-	case load_balancer:start_link(Module, Func, Max) of
-		{ok, Balance} ->
-				irc_log:logVerbose("Started " ++ LogInfo ++ " load balancer"),
-				Balance;
-
-		_ ->	irc_log:logFatal( "Cannot start " ++ LogInfo ++ " Load balancer, abort"),
-				halt()
-	end.
 
 init( Args ) ->
 	{ok, { { one_for_one, 1000, 3600 },
@@ -60,10 +52,15 @@ start( _StartType, _StartArgs ) ->
 	MaxChan = 10,	% load real constant from conf
 	CliBalance = make_specbalance( load_balancer, start_link,
 									[client_listener, start_link, MaxCli] ),
+	supervisor:start_child( RootSupervisor, CliBalance ),
 	irc_log:logVerbose( "Client balance launched" ),
 	ChanBalance = make_specbalance( load_blancer, start_link,
-									[chan_lanager, start_link, MaxCha] ),
+									[chan_lanager, start_link, MaxChan] ),
+	supervisor:start_child( RootSupervisor, ChanBalance ),
 	irc_log:logVerbose( "Chan balance launched" ),
+	ServerNode = make_specserv( server_node, start_link, [RootSupervisor, CliBalance, ChanBalance] ),
+	supervisor:start_child( RootSupervisor, ServerNode ),
+	irc_log:logVerbose( "Server node launched" ),
 	ok.
 
 start_phase( _Phase, _StartType, _PhaseArgs ) ->
--- src/server_node.erl
+++ src/server_node.erl
@@ -4,6 +4,7 @@
 
 -record( srvs,
 		{
+			supervisor,
 			clibal,		% load balancer for client
 			chanbal	% load balancer for channels
 		} ).
@@ -12,8 +13,47 @@
 			is_client_existing/2
 		]).
 
+% export for the gen_server
+-export([init/1,
+		start_link/3,
+		handle_call/3,
+		handle_cast/2,
+		handle_info/2,
+		terminate/2,
+		code_change/3]).
+
 -vsn( p01 ).
 
-is_client_existing( ServerPid, NickName ) ->
+is_client_existing( _ServerPid, _NickName ) ->
 	false.
+
+start_link(Supervisor, CliBalance, ChanBalance) ->
+	gen_server:start_link(?MODULE, [{Supervisor, CliBalance, ChanBalance}], [] ).
+		
+%%
+% gen_server implementation
+%%
+init( {Supervisor, Cli, Chan} ) ->
+	State = #srvs{supervisor=Supervisor,
+					clibal = Cli,
+					chanbal = Chan },
+	{ok, State}.
+
+handle_call( _What, _From, State ) ->
+	{noreply, State}.	
 	
+%
+% Different call used by the load balancer.
+%
+handle_cast( _Command, State ) ->
+	{noreply, State}.
+	
+handle_info(_What, State) ->
+	{noreply, State}.
+
+terminate(_Reason,State) ->
+	{ok, State}.
+
+code_change(_OldVsn, State,_Extra) ->
+	{ok, State}.
+
