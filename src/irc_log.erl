59079815123fc4064497fe5f0fb70fcf689fbbab
vincent <vincent@fd6709c4-d1e2-46c1-9206-fa2d5ecda316>
2008-03-25 20:07:15 +0000

    Added many comments to irc_log
    git-svn-id: svn://127.0.0.1/jim/trunk@82 fd6709c4-d1e2-46c1-9206-fa2d5ecda316

--- src/irc_log.erl
+++ src/irc_log.erl
@@ -1,3 +1,10 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% @doc
+%%	This module represent the logging thread which
+%%	store all the diferents elements which happen
+%%	across the server
+%% @end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -module (irc_log).
 
 -define( LogVerbose, 0).
@@ -21,10 +28,27 @@
 
 -vsn( p01 ).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% @doc
+%%	Start a basic logger which output everything
+%%	on the standard output.
+%% @end
 basic_init() ->
 	init( (fun(_,Txt) -> io:put_chars(Txt) end),
 			[] ).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% @doc
+%%	Start a logger. It's a more advanced one,
+%%	every logging events are passed through a
+%%	given user function.
+%% @end
+%%
+%% @spec init( Function, FuncArg ) -> Result
+%% where
+%%		Function = func()
+%%		FuncArg = term()
+%%		Result = {ok, Pid} | {error, Reason}
 init( Function, FuncArg ) ->
 	LogStatus = whereis( irc_serv_logger ),
 	if 
@@ -32,24 +56,77 @@ init( Function, FuncArg ) ->
 			Pid = spawn( ?MODULE, logthread, [Function, FuncArg] ),
 			register( irc_serv_logger, Pid ),
 			{ok, Pid};
-		true -> error
+		true -> {error, "Problem while launching logger thread"}
 	end.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% @doc
+%%	Log marginaly informative messages, used
+%%	for debug, or to display inner information
+%%	about the server.
+%% @end
+%%
+%% @spec logVerbose( Txt ) -> nil
+%% where
+%%		Txt = string()
 logVerbose( Txt ) ->
 	logmsg( ?LogVerbose, Txt ).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% @doc
+%%	Log some milestones usefull for administration
+%%	eg : begin/end of initialisation.
+%% @end
+%%
+%% @spec logInfo( Txt ) -> Result
+%% where
+%%		Txt = string()
+%%		Result = none
 logInfo( Txt ) ->
 	logmsg( ?LogInfo, Txt ).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% @doc
+%%	Log information like "a client has connected"
+%%	and information needed by administration (conf
+%%	reloading is a good exemple).
+%% @end
+%%
+%% @spec logEvent( Txt ) -> nil
+%% where
+%%		Txt = string()
 logEvent( Txt ) ->
 	logmsg( ?LogEvent, Txt ).
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% @doc
+%%	If an event is abnormal and cause a thread
+%%	crash, it should be this log level.
+%% @end
+%%
+%% @spec logError( Txt ) -> nil
+%% where
+%%		Txt = string()
 logError( Txt ) ->
 	logmsg( ?LogError, Txt ).
 	
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% @doc
+%%	Log information issued before crashing the
+%%	the server, should be followed by a call to
+%%	the halt() function.
+%% @end
+%%
+%% @spec logFatal( Txt ) -> nil
+%% where
+%%		Txt = string()
 logFatal( Txt ) ->
 	logmsg( ?LogFatal, Txt ).
-	
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 logmsg( ?LogVerbose, Txt ) ->
 	Logger = whereis( irc_serv_logger ),
 	Logger!{?LogVerbose, ": " ++ Txt};
@@ -70,6 +147,11 @@ logmsg( ?LogFatal, Txt ) ->
 	Logger = whereis( irc_serv_logger ),
 	Logger!{?LogFatal, "! " ++ Txt}.
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% @doc
+%%	Transform a date/time information to
+%%	a textual representation
+%% @end
 getTime() ->
 	{ {Year, Month, Day},
 		{Hour,Minute,Second} } = erlang:localtime(),
