eda5a1f05294871a1088128b8fef253c88559bc2
nicolas <nicolas@fd6709c4-d1e2-46c1-9206-fa2d5ecda316>
2008-03-26 12:17:25 +0000

    add \n to solve a minor problem.
    git-svn-id: svn://127.0.0.1/jim/trunk@86 fd6709c4-d1e2-46c1-9206-fa2d5ecda316

--- include/irc_struct.hrl
+++ include/irc_struct.hrl
@@ -39,12 +39,12 @@
 
 %% Defining EIS responses during
 %% connection of a client.
--define( LOOKING_HOST_MSG, ":*** Looking up your hostname..." ).
--define( HOST_FOUND_MSG, ":*** Found your hostname." ).
--define( HOST_NOT_FOUND_MSG, ":*** Unable to find your hostname, closing connection." ).
--define( BAD_SEQUENCE_MSG, ":***Bad commands sequence." ).
--define( TIME_OUT_MSG, ":*** Time out, closing connection." ).
--define( VALIDATION_MSG, ":*** You've been regitered !" ).
+-define( LOOKING_HOST_MSG, ":*** Looking up your hostname...\n" ).
+-define( HOST_FOUND_MSG, ":*** Found your hostname.\n" ).
+-define( HOST_NOT_FOUND_MSG, ":*** Unable to find your hostname, closing connection.\n" ).
+-define( BAD_SEQUENCE_MSG, ":***Bad commands sequence.\n" ).
+-define( TIME_OUT_MSG, ":*** Time out, closing connection.\n" ).
+-define( VALIDATION_MSG, ":*** You've been regitered !\n" ).
 
 
 -record(chan,
