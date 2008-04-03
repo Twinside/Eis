8fa65abb8032fc7395b17807f0d8a35d64dd8323
vince <vince@fd6709c4-d1e2-46c1-9206-fa2d5ecda316>
2008-04-03 16:46:43 +0000

    com_notice.erl fait -> A tester
    git-svn-id: svn://127.0.0.1/jim/trunk@116 fd6709c4-d1e2-46c1-9206-fa2d5ecda316

--- src/commands/com_notice.erl
+++ src/commands/com_notice.erl
@@ -3,6 +3,17 @@
 %%	to the IRC notice command.
 %% @end
 -module( com_notice ).
-
 -vsn( p01 ).
 
+-export	( 
+		[notice/2]
+		).
+
+notice(Msg, Dest) ->
+	Lst = Msg#msg.params,
+	
+	case Lst of
+		[Sender | Text] ->
+					(Dest#client.send)((Sender#client.sendArg), Text)
+	end
+.
