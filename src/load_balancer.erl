74fa2d34f4be91baafdaa045134c8f96c65bc5d9
vincent <vincent@fd6709c4-d1e2-46c1-9206-fa2d5ecda316>
2008-04-09 10:27:57 +0000

    ChangeAY load_balancer qui devrait maintenant marcher, mais a debugger
    git-svn-id: svn://127.0.0.1/jim/trunk@136 fd6709c4-d1e2-46c1-9206-fa2d5ecda316

--- src/load_balancer.erl
+++ src/load_balancer.erl
@@ -6,33 +6,40 @@
 %% @end
 -module (load_balancer).
 
--behaviour(supervisor).
+-behaviour( supervisor ).
+-behaviour( gen_server ).
 
 % export for the behaviour.
 -export([init/1]).
 
-
+% export for the gen_server
 -export([
-			start_link/3,		% to launch a blancer.
-			add_ressource/2,	% helper to add a ressource
-			kill_ressource/2,	% helper to add a ressource.
-			balancer/2
+			%init/1,
+			handle_call/3,
+			handle_cast/2,
+			handle_info/2,
+			terminate/2,
+			code_change/3
 		]).
 
 -export([
-			bootstrap_balancer/0
+			start_link/3		% to launch a blancer.
+			,add_ressource/2	% helper to add a ressource
+			,kill_ressource/2	% helper to add a ressource.
 		]).
 
 -record( bconf,
 		{
 			maxress,	% maximum of client per process
-			spec,	% general process specification
-			curr	% current process count.
+			spec,		% general process specificatioun
+			suppid,		% pid of the supervisor
+			count = 1,		% current process count.
+			children = []	% list of... children...
 		}).
 		
 -record( pinfo,
 		{
-			count,	% as integer
+			count = 0,	% as integer
 			proc,	% as Pid
 			spec	% as childspec (supervisor)
 		}).
@@ -52,7 +59,7 @@
 %%		Function = atom()
 %%		MaxRessource = integer()
 start_link(Module, Function, {MaxRessource, Referer}) ->
-	Balance = spawn(?MODULE, bootstrap_balancer,[]),
+	{ok, Balance} = gen_server:start_link( ?MODULE, [ {serverinit, unit} ], [] ),
 	InitialProcess = {0,
 						{
                             Module,
@@ -60,11 +67,11 @@ start_link(Module, Function, {MaxRessource, Referer}) ->
                             [ {Balance, Referer} ]
                          },
 						permanent, 1000, worker, [Module]},
-	Conf = #bconf{ maxress= MaxRessource,
-				   spec=InitialProcess,
-				   curr=1 },
 	case supervisor:start_link(?MODULE, InitialProcess ) of
-		{ok, Pid} -> Balance!{notifysupervisor, Pid, {Conf, [InitialProcess]}},
+		{ok, Pid} -> Conf = #bconf{ maxress= MaxRessource,
+									suppid = Pid,
+								   spec=InitialProcess },
+					 gen_server:cast( Balance, Conf ),
 					 {ok, Balance};
 
 		{error, _} -> irc_log:logFatal("Cannot start LoadBlancer"),
@@ -82,11 +89,7 @@ start_link(Module, Function, {MaxRessource, Referer}) ->
 %%		Rsrc = term()
 %%		Result = {ok, Pid} | {error, Reason}
 add_ressource( BalancerPid, Rsrc ) ->
-	BalancerPid!{addressource,Rsrc, self()},
-	receive
-		{putted, Pid} -> {ok, Pid};
-		Otherwise -> Otherwise
-	end.
+	gen_server:call( BalancerPid, {addressource, Rsrc } ).
 
 %% @doc
 %%	Remove a ressource from the balance. 
@@ -96,70 +99,62 @@ add_ressource( BalancerPid, Rsrc ) ->
 %%		BalancerPid	= pid()
 %%		Rsrc = term()
 kill_ressource( BalancerPid, Rsrc ) ->
-	BalancerPid!{killressource, Rsrc}.
+	gen_server:cast( BalancerPid, {killressource, Rsrc} ).
 
 %
 % Update the balanced process list to
 % get the new ressource.
 %
-update_process( Count, [First | Next], New, Parent ) ->
-	if First#pinfo.count == Count ->
-			gen_server:cast(First#pinfo.proc, {addressource, New}),
-			Parent!{putted, First#pinfo.proc},
-			[#pinfo{count = Count + 1,
-					proc = First#pinfo.proc,
-					spec = First#pinfo.spec} |Next];
-		true -> [First | update_process(Count, Next, New, Parent)]
-	end.
+update_process( Count, Prev, [First | Next], New, State ) 
+	when First#pinfo.count == Count->
 
+	gen_server:cast(First#pinfo.proc, {addressource, New}),
+	NewPinfo = First#pinfo{ count = Count + 1 },
+	NeoState = State#bconf{ children =  Prev ++ [NewPinfo |Next] },
+	{NewPinfo#pinfo.proc, NeoState};
+
+update_process( Count, Prev, [First|Next], New, State ) ->
+	update_process(Count, [First| Prev], Next, New, State).
+	
 %
 % In charge of adding a ressource.
 % SuperPid : pid of the supervisor
 % Conf : configuration state of the balancer.
 % ChildList : list of balanced process
 % New : ressource to add.
-ressource_adding( SuperPid, Conf, ChildList, New, Parent ) ->
+ressource_adding( State, Rsrc ) ->
 	SmallestProc = (fun(Proc,Min) -> PMin = Proc#pinfo.count,
 									 if Min < PMin -> Min;
 										 true -> PMin end end),
 
-	MinRes = lists:foldl( SmallestProc, ChildList, 999999 ),
+	MinRes = lists:foldl( SmallestProc, State#bconf.children, 999999 ),
 	
-	if MinRes >= Conf#bconf.maxress ->	% all our threads are full, launch a new one.
-			Spec = setelement(1, Conf#bconf.spec, Conf#bconf.curr),
-			{ok, Pid} = supervisor:start_child(SuperPid, Spec),
-			gen_server:cast(Pid, {addressource, New}),
-			Parent!{putted, Pid},
+	if MinRes >= State#bconf.maxress ->	% all our threads are full, launch a new one.
+			Spec = setelement(1, State#bconf.spec, erlang:make_ref() ),
+			{ok, Pid} = supervisor:start_child(State#bconf.suppid, Spec),
+			gen_server:cast(Pid, {addressource, Rsrc}),
 			NewProcess = #pinfo{count=1, proc=Pid, spec=Spec},
-			NewConf = setelement(3, Conf, Conf#bconf.curr + 1),
-			[First | Next] = ChildList,			% used to recombine the new list.
-			{NewConf, [NewProcess,First|Next]};
+			NewConf = State#bconf{ children = [NewProcess] ++ State#bconf.children,
+									count = State#bconf.count + 1 },
+			{Pid, NewConf};
 			
-		true -> {Conf, update_process( MinRes, ChildList, New, Parent )}
+		true -> update_process( MinRes, [], State#bconf.children, Rsrc, State )
 	end.
 
 
 % decrement ressource count of a process.
-dec_count( _Pid, [] ) -> [];
-dec_count( Pid, [P | Next] ) ->
-	if P#pinfo.proc == Pid -> N = #pinfo { count = P#pinfo.count - 1,
-											proc = Pid,
-											spec = P#pinfo.spec },
-							[N | Next];
-		true -> [P | dec_count( Pid, Next )]
-	end.
+dec_count( _Pid, _Prev, [], State ) ->
+	irc_log:logError( "Ressource killed by a non-balance" ),
+	State;
 
-%% @doc
-%%  Wait to receive the state of the balancer,
-%%  and then launch it.
-%% @end
-%% @hidden
-bootstrap_balancer() ->
-	receive
-		{notifysupervisor, Pid, State} -> balancer( Pid, State ); 
-		_ -> irc_log:logFatal( "Wrong bootstraping of load balancer, halting"),
-			halt()
-	end.
+dec_count( Pid, Prev, [P | Next], State )
+	when P#pinfo.proc == Pid ->
+
+	Upd = P#pinfo{ count = P#pinfo.count - 1 },
+	State#bconf{ children =	Prev ++ [Upd | Next] };
+
+dec_count( Pid, Prev, [P | Next], State ) ->
+	dec_count( Pid, [P|Prev], Next, State ).
 
 %% @doc
 %%  Thread keeping state of the process
@@ -167,26 +162,36 @@ bootstrap_balancer() ->
 %% @end
 %% @todo : add a suicide message or something.
 %% @hidden
-balancer( SuperPid, {Conf, ChildList} ) ->
-	receive
-		{addressource, Rsc, From} ->
-			State = ressource_adding(SuperPid, Conf, ChildList, Rsc, From ),
-			load_balancer:balancer( SuperPid, State );
+handle_call( {addressource, Rsc}, _From, State ) ->
+	{Assigned, NeoState} = ressource_adding( State, Rsc ),
+	{reply, Assigned, NeoState};
 
-		{killressource, Rsc} ->
-			_ = lists:foldl( (fun(Proc,What) -> gen_server:cast(Proc#pinfo.proc, What), What end),
-							{killressource, Rsc}, ChildList ),
-			load_balancer:balancer( SuperPid, {Conf, ChildList});
-		
-		{killedaressource, Pid} ->
-			load_balancer:balancer( SuperPid, {Conf, dec_count(Pid, ChildList)} );
+handle_call( _, _, State ) ->
+	irc_log:logError( "Unrecognized call to load balancer" ),
+	{noreply, State}.
 
-		_ -> error
-	end.
+handle_cast( {killressource, Rsc}, State ) ->
+	NeoState = lists:foldl( (fun(Proc,What) -> gen_server:cast(Proc#pinfo.proc, What), What end),
+					{killressource, Rsc}, State#bconf.children ),
+	{noreply, NeoState};
 
+% Used to set the state of ther server at
+% the begining of it's lifetime.
+handle_cast( State, void ) ->
+	{noreply, State};
 
-%% used by the supervisor behaviour.
+handle_cast( {killedaressource, Pid}, State ) ->
+	{noreply, dec_count( Pid, [], State#bconf.children, State )};
+
+handle_cast( _, State ) ->
+	irc_log:logError( "Unrecognized balancer call" ),
+	{noreply, State}.
+
+%% used by the supervisor and gen_server behaviour.
 %% @hidden
+init( [{serverinit, unit}] ) ->
+	{ok, void};
+
 init( IniChild ) ->
 	ok = supervisor:check_childspecs( [IniChild] ),
 	{ok,
@@ -194,4 +199,8 @@ init( IniChild ) ->
 			{one_for_one, 1000,3600},% if more than 1 process stop in 60 seconds.
 			[IniChild]
 		}}.
+
+handle_info( _, _ ) -> undefined.
+terminate( _, _ ) -> undefined.
+code_change( _, _, _) -> undefined.
 
