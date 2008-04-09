957e1a437b44f1c61081356ed0cd5c223529742b
vince <vince@fd6709c4-d1e2-46c1-9206-fa2d5ecda316>
2008-04-09 13:34:03 +0000

    conf_loader ok (mais pas fini)
    git-svn-id: svn://127.0.0.1/jim/trunk@143 fd6709c4-d1e2-46c1-9206-fa2d5ecda316

--- src/conf_loader.erl
+++ src/conf_loader.erl
@@ -24,17 +24,24 @@ loadConf(Device, Conf) ->
 			file:close(Device), 
 			Conf;
 		Line -> 
+			case parse_line( Line ) of
+				[Key, Val|_] -> loadConf( Device, [{Key,Val} | Conf] );
+				_ -> loadConf( Device, Conf )
+			end
 			%% Supprime les " ", \n,le "=" et les "
-			loadConf(Device, [string:tokens(
-								string:strip(
-									string:strip(
-										string:strip(Line, both, $\n),
-										both, $=),
-									both, $"),
-								" =")|Conf])
 	end.
 
-getElement( [ [Name|Val] | Queue ], Seek ) ->
+parse_line( [$#  | _] ) -> none;
+parse_line( [$\r | _] ) -> none;
+parse_line( [$\n | _] ) -> none;
+parse_line( Line ) ->
+	Endcleaned = string:strip(Line, both, $\n),
+	Cuted = string:strip( Endcleaned, both, $  ),
+	Dequoted = string:strip( Cuted, both, $"),
+	string:tokens( Dequoted, "=").
+
+%% @hidden
+getElement( [ {Name, Val} | Queue ], Seek ) ->
 	if Name == Seek ->
 		Val;
 	true -> 
@@ -44,6 +51,9 @@ getElement( [ [Name|Val] | Queue ], Seek ) ->
 getElement( [], _Seek) ->
 	not_found.
 
+%% @doc
+%% Recherche la valeur d'une configuration
+%% @end
 getElement( Name ) ->
 	case whereis(conf_loader) of
 		undefined ->
@@ -52,14 +62,18 @@ getElement( Name ) ->
 			gen_server:call( Pid, {get, Name} )
 	end.
 
-
+%% @doc
+%% Creer un processus pour le conf_loader et charge la configuration
+%% @end
 start_link( FileName ) ->
 	gen_server:start_link( ?MODULE, FileName, [] ).
 
+%% @hidden
 init( FileName ) ->
 	register( conf_loader, self() ),
 	{ok, loadConf( FileName )}.
 
+%% @hidden
 handle_call( {get, Name}, _From, Conf ) ->
 	Reply = getElement(Conf, Name),	
 	{reply, Reply, Conf};
@@ -75,16 +89,16 @@ handle_call( {reload, FileName}, _From, _Conf ) ->
 handle_call( {save, _FileName}, _From, Conf ) ->
 	%% Sauver la conf
 	{noreply, Conf}.
-     
+%% @hidden     
 handle_cast( _Command, State ) ->
 	{noreply, State}.
-	
+%% @hidden	
 handle_info(_What, State) ->
 	{noreply, State}.
-
+%% @hidden
 terminate(_Reason,State) ->
 	%% Sauver la conf	
 	{ok, State}.
-
+%% @hidden
 code_change(_OldVsn, State,_Extra) ->
 	{ok, State}.
