3fe1d2763b9648d136157bac1a091dc7f024b52f
vince <vince@fd6709c4-d1e2-46c1-9206-fa2d5ecda316>
2008-04-24 13:36:10 +0000

    git-svn-id: svn://127.0.0.1/jim/trunk@165 fd6709c4-d1e2-46c1-9206-fa2d5ecda316

---
+++ src/commands/com_topic.erl
@@ -0,0 +1,27 @@
+-module( com_topic ).
+
+-include( "irc_struct.hrl" ).
+
+-vsn( p01 ).
+
+%%-export([
+%%            perform_client/3
+%%            ,perform_chan/4
+%%        ]).
+
+%%perform_client( Msg, Cli, ClientState ) ->
+%%	Tosend = prepare_sended_text( Msg, Cli ),
+%%	Dispatcher = (fun( {Chan,Pid}, _ ) ->
+%%                  chan_manager:send_chan( Pid, {Msg, Chan, {Tosend, Cli}} )
+%%                   end),
+%% 	lists:foldl( Dispatcher, 0, Cli#client.is_in ),
+%%        ClientState.
+
+%%perform_chan( Msg, {MsgTosend, User}, Chan, ChanState ) ->
+%%	CheckRight = check_granting( ?MCHANOP, chan_manager:get_user_right( Chan, Cli#client.nick ) ),	
+%%	if Checkright ->
+%%		[ _Dest| _Command | [Subject | _] ] = Msg#msg.params,
+%%		Ok = irc:is_channame_valid(Chan#chan.channame),
+
+	%% PAS FINIT
+%%	end.
